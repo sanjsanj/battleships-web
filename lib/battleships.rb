e80b7c505b99b189c560443c5844d37ba5984112
M1lena <stoevami@gmail.com>
2015-04-01 10:25:03 +0100

    conditional attempt

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -8,14 +8,23 @@ class BattleShips < Sinatra::Base
     erb :index
   end
 
-  get '/new_game' do
-    erb :new_game
+  get('/new_game') do erb :new_game
   end
 
   post '/new_game' do
     @name = params[:Name]
-    erb :new_game
+
+    # if @name is empty redirect the player to same thing again, but this time send a message as well
+    # else welcome the player
+
+    if @name.empty?
+      @message = 'Dont forget to enter your name'
+      erb :new_game
+    else
+      "Welcome #{@name}"
+    end
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
